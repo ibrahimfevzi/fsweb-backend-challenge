5cb463060cfbc31e95d5ff757a87eb95caf6e0d9
ibrahimfevzi <ibrahimfevzi@gmail.com>
2023-06-04 22:55:01 +0300

    update

--- api/comments/comments-router.js
+++ api/comments/comments-router.js
@@ -4,18 +4,90 @@ const commentModel = require("./comments-model");
 const authMiddleware = require("../auth/auth-middleware");
 
 // GET /comments
-router.get("/", commentModel.getAllComments);
+router.get("/", async (req, res) => {
+  try {
+    const comments = await commentModel.getAllComments();
+    res.json(comments);
+  } catch (error) {
+    res.status(500).json({ message: "Yorumları getirirken bir hata oluştu." });
+  }
+});
 
 // GET /comments/:id
-router.get("/:id", commentModel.getCommentById);
+router.get("/:id", commentModel.getCommentById, async (req, res) => {
+  const commentId = req.params.id;
+  try {
+    const comment = await commentModel.getCommentById(commentId);
+    if (comment) {
+      res.json(comment);
+    } else {
+      res.status(404).json({ message: "Yorum bulunamadı." });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Yorumu getirirken bir hata oluştu." });
+  }
+});
 
 // POST /comments
-router.post("/", authMiddleware.authenticate, commentModel.createComment);
+router.post(
+  "/",
+  //   authMiddleware.authenticate,
+  commentModel.createComment,
+  async (req, res) => {
+    const { content } = req.body;
+    const { user_id } = req.token;
+    const { post_id } = req.body;
+    const comment = { content, user_id, post_id };
+    try {
+      const newComment = await commentModel.createComment(comment);
+      res.status(201).json(newComment);
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "Yorum oluşturulurken bir hata oluştu." });
+    }
+  }
+);
 
 // PUT /comments/:id
-router.put("/:id", authMiddleware.authenticate, commentModel.updateComment);
+router.put(
+  "/:id",
+  //   authMiddleware.authenticate,
+  commentModel.updateComment,
+  async (req, res) => {
+    const commentId = req.params.id;
+    const { content } = req.body;
+    const { user_id } = req.token;
+    const { post_id } = req.body;
+    const comment = { content, user_id, post_id };
+    try {
+      const updatedComment = await commentModel.updateComment(
+        commentId,
+        comment
+      );
+      res.status(200).json(updatedComment);
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "Yorum güncellenirken bir hata oluştu." });
+    }
+  }
+);
 
 // DELETE /comments/:id
-router.delete("/:id", authMiddleware.authenticate, commentModel.deleteComment);
+router.delete(
+  "/:id",
+  //   authMiddleware.authenticate,
+  commentModel.deleteComment,
+  async (req, res) => {
+    const commentId = req.params.id;
+    try {
+      const deletedComment = await commentModel.deleteComment(commentId);
+      res.status(200).json(deletedComment);
+    } catch (error) {
+      res.status(500).json({ message: "Yorum silinirken bir hata oluştu." });
+    }
+  }
+);
 
 module.exports = router;
--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -4,18 +4,82 @@ const postModel = require("./posts-model");
 const authMiddleware = require("../auth/auth-middleware");
 
 // GET /posts
-router.get("/", postModel.getAllPosts);
+
+router.get("/", async (req, res) => {
+  try {
+    const posts = await postModel.getAllPosts();
+    res.json(posts);
+  } catch (error) {
+    res.status(500).json({ message: "Postları getirirken bir hata oluştu." });
+  }
+});
 
 // GET /posts/:id
-router.get("/:id", postModel.getPostById);
+router.get("/:id", async (req, res) => {
+  const postId = req.params.id;
+  try {
+    const post = await postModel.getPostById(postId);
+    if (post) {
+      res.json(post);
+    } else {
+      res.status(404).json({ message: "Post bulunamadı." });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Postu getirirken bir hata oluştu." });
+  }
+});
 
 // POST /posts
-router.post("/", authMiddleware.authenticate, postModel.createPost);
+router.post(
+  "/",
+  //   authMiddleware.authenticate,
+  postModel.createPost,
+  async (req, res) => {
+    const { content } = req.body;
+    const { user_id } = req.token;
+    const post = { content, user_id };
+    try {
+      const newPost = await postModel.createPost(post);
+      res.status(201).json(newPost);
+    } catch (error) {
+      res.status(500).json({ message: "Post oluşturulurken bir hata oluştu." });
+    }
+  }
+);
 
 // PUT /posts/:id
-router.put("/:id", authMiddleware.authenticate, postModel.updatePost);
+router.put(
+  "/:id",
+  //   authMiddleware.authenticate,
+  postModel.updatePost,
+  async (req, res) => {
+    const postId = req.params.id;
+    const { content } = req.body;
+    const { user_id } = req.token;
+    const post = { content, user_id };
+    try {
+      const updatedPost = await postModel.updatePost(postId, post);
+      res.json(updatedPost);
+    } catch (error) {
+      res.status(500).json({ message: "Post güncellenirken bir hata oluştu." });
+    }
+  }
+);
 
 // DELETE /posts/:id
-router.delete("/:id", authMiddleware.authenticate, postModel.deletePost);
+router.delete(
+  "/:id",
+  //   authMiddleware.authenticate,
+  postModel.deletePost,
+  async (req, res) => {
+    const postId = req.params.id;
+    try {
+      const deletedPost = await postModel.deletePost(postId);
+      res.json(deletedPost);
+    } catch (error) {
+      res.status(500).json({ message: "Post silinirken bir hata oluştu." });
+    }
+  }
+);
 
 module.exports = router;
