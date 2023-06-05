1a177efe9b2a766cede36a0577578735131e2573
ibrahimfevzi <ibrahimfevzi@gmail.com>
2023-06-05 12:19:13 +0300

    update

--- api/server.js
+++ api/server.js
@@ -4,11 +4,23 @@ const usersRouter = require("./users/users-router");
 const authRouter = require("./auth/auth-router");
 const commentsRouter = require("./comments/comments-router");
 const postsRouter = require("./posts/posts-router");
+const helmet = require("helmet");
+const cors = require("cors");
+const morgan = require("morgan");
 
+//global middleware
 server.use(express.json());
+server.use(helmet());
+server.use(cors());
+server.use(morgan("dev"));
+
+//routes
 server.use("/api/users", usersRouter);
 server.use("/api/auth", authRouter);
 server.use("/api/comments", commentsRouter);
 server.use("/api/posts", postsRouter);
 
+//error handling middleware
+
+//export
 module.exports = server;
